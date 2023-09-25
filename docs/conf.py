a1e1fd83415c2549e00bf3b5ef648613c8f136dd
owari-taro <taro.biwajima@gmail.com>
2023-09-26 07:21:01 +0900

    add nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,8 @@ extensions = [
     'sphinx.ext.todo', 
     'sphinx_design',
     "sphinx_last_updated_by_git"
+        'nbsphinx',
+
 ]
 
 
@@ -56,7 +58,7 @@ language = "jp"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["venv","_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["venv","_build", "Thumbs.db", ".DS_Store",'**.ipynb_checkpoints']
 
 # -- Options for HTML output -------------------------------------------------
 
