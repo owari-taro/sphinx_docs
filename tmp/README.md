8efbcd592c5bac548102faaf232e34ec31241387
owari-taro <58625526+owari-taro@users.noreply.github.com>
2023-11-06 16:59:44 +0900

    tmp

---
+++ tmp/README.md
@@ -0,0 +1,4 @@
+```
+>git_repos/sphinx_docs/tmp$ docker build -t dummy-conda .
+>git_repos/sphinx_docs/tmp$ docker run --rm -it dummy-conda /bin/bash
+```
---
+++ tmp/script.py
@@ -0,0 +1,7 @@
+from osgeo import gdal
+import requests
+
+print(requests.get("https://google.com"))
+print(requests.__doc__)
+print("++++++++++++++++++++++++++++++++++")
+print(gdal.Open.__doc__)
